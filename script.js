eb8ed1fe32fd53916b3aebde6978c1ace7012ac6
Enrico <enricocavaliere56@gmail.com>
2025-10-31 09:21:56 -0300

    Js
    tentando fazer o js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sobreLink = document.getElementById("sobre-link");
+  sobreLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.href = "sobre.html"; 
+  });
+});
