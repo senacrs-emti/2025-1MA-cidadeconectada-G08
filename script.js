1e50578f9e5022838b80bd663adb7e9d7f16d3e8
Samuel Cunda <cundasamuel@gmail.com>
2025-10-31 12:26:16 -0300

    Mapa
    Fiz o mapa principal do site

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-let local = navigator.geolocation.getCurrentPosition;
-console.log(local);
+let h2 = document.querySelector('h2');
+var map;
+console.log(map)
+
+function success(pos){
+    console.log(pos.coords.latitude, pos.coords.longitude);
+    h2.textContent = `Latitude:${pos.coords.latitude}, Longitude${pos.coords.longitude}`;
+
+    var map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 18);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
+        .bindPopup('Você está aqui!!')
+        .openPopup();
+}
+
+function error(err){
+    console.log(err)
+}
+ 
+var watchID = navigator.geolocation.watchPosition(success, error, {
+    enableHighAccuracy: true,
+    timeout: 5000
+});
+
