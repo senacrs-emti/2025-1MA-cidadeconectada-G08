48572a2faab2e4e82832e9bd6bb55ab7c1f9dd8d
Enrico <enricocavaliere56@gmail.com>
2025-11-03 12:13:33 -0300

    arrumado
    eu tinha salvado errado e tinha feita erros e agora foi arrumado

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const sobreLink = document.getElementById("sobre-link");
   sobreLink.addEventListener("click", (event) => {
     event.preventDefault();
-    window.location.href = "sobre.html"; 
+    window.location.href = "./sobre.html"; 
   });
 });
