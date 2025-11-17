761779c062497d757df957bfaef7fdc5732943fb
Enrico <enricocavaliere56@gmail.com>
2025-11-17 10:53:12 -0300

    click
    sendo feito o click

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+/*
 document.addEventListener("DOMContentLoaded", () => {
   const sobreLink = document.getElementById("sobre-link");
   sobreLink.addEventListener("click", (event) => {
     event.preventDefault();
     window.location.href = "./sobre.html"; 
   });
-});
+});*/
