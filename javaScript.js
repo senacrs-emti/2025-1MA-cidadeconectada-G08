8164ea30fb8c1d5a95b9284bbeee7b784de30e5b
JIsaquewillers <joaoisaquewillers@gmail.com>
2025-11-17 12:17:29 -0300

    javascript
    começei o javascript

---
+++ javaScript.js
@@ -0,0 +1,8 @@
+    const textoEscondido = document.getElementById("p1")
+    const div = document.getElementById("Quadrado-branco1")
+    
+    const btnLeiaMais = document.getElementById("leia-mais");
+    btnLeiaMais.addEventListener("click",
+        function(){
+    
+    })
