37e6caf034cd3d390f8d2fe497f0c1383c1a50f5
JIsaquewillers <joaoisaquewillers@gmail.com>
2025-11-19 12:12:44 -0300

    ajustes

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,7 @@
-    const textoEscondido = document.getElementById("texto-escondido");
+   //crio uma variável para cada elemento
+   const textoEscondido = document.getElementById("texto-escondido");
     const div = document.getElementById("Quadrado-branco1");
-  const btnLeiaMais = document.getElementById("leia-mais");
+    const btnLeiaMais = document.getElementById("leia-mais");
     
 
     //quando clica no botão leia mais
@@ -8,10 +9,10 @@
         //cria uma função
         function(){;
             //se o display do texto escondido no css for "none"
-            //quando clicar no botão irá se tornar "block", e denominará a div height como auto
+            //quando clicar no botão irá se tornar "block", e denominará a div height como 600px
            if(textoEscondido.style.display === "none"){;
             (textoEscondido.style.display = "block");
-            (div.style.width = "400px");
+            (div.style.height = "600px");
             (btnLeiaMais.innerHTML ="leia menos" );
            }
            //senão, quando clicar no botão novamente, irá retomar tudo ao normal
@@ -21,4 +22,3 @@
            };
     })
 
-    //nao sei por que nao está funcionando
